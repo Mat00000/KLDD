77a4a77cf1dd8769f1e11a9fa8dc05acbebe56c4
dudusv2 <34479833+dudusv2@users.noreply.github.com>
2018-12-14 13:07:03 +0100

    Add files via upload

--- Program_Java/JAVA/Bus.java
+++ Program_Java/JAVA/Bus.java
@@ -9,25 +9,25 @@ public class Bus {
     int left, right, top, bottom;
     point center;
     double vector;
-    int line;
-
-    public Bus(int left, int top, int right, int bottom,int line) {
+    int life_time;
+    //Construktor klasy bus 
+    public Bus(int left, int top, int right, int bottom, int life_time) {
         this.left = left;
         this.right = right;
         this.top = top;
         this.bottom = bottom;
-        this.line = line;
+        this.life_time = life_time;
         center = center();
         vector = vector();
 
     }
-
+    // Nadaje środek bounding box'a busa
     public final point center() {
-         System.out.println("Środek x="+(right + left)/2+" y="+ (bottom + top) / 2);
+        System.out.println("Środek x=" + (right + left) / 2 + " y=" + (bottom + top) / 2);
         return new point((right + left) / 2, (bottom + top) / 2);
-       
-    }
 
+    }
+    // Nadaje połowa przekątnej bounding bus'a
     public final double vector() {
         return Math.sqrt((double) (right - center.x) * (right - center.x) + (top - center.y) * (top - center.y));
     }
--- Program_Java/JAVA/Yolo_project.java
+++ Program_Java/JAVA/Yolo_project.java
@@ -12,11 +12,11 @@ import java.util.Scanner;
  */
 public class Yolo_project {
 
-    int current, counter;
-    int delta = 50, but_lifetime = 80;
-    ArrayList<Bus> c_buses = new ArrayList<>();
-    ArrayList<Bus> buses = new ArrayList<>();
-    ArrayList<Bus> r_buses = new ArrayList<>();
+    int delta = 50;  //odległość miedzy środkami 
+    int bus_lifetime = 50; // czas życia busa 
+    ArrayList<Bus> c_buses = new ArrayList<>(); //Busy w pamięci
+    ArrayList<Bus> buses = new ArrayList<>();   //Busy które przejechały (zliczone)
+    ArrayList<Bus> r_buses = new ArrayList<>(); // Busy do usunięcia 
     boolean flag = false;
 
     public static void main(String[] args) {
@@ -25,39 +25,42 @@ public class Yolo_project {
         int i = 0;
         for (Bus b : yolo.buses) {
             i++;
-            System.out.println("Hej zliczyłem skurczysyna " + i);
-            System.out.println("Bus ");
+            System.out.println("Bus "+i);
         }
     }
 
+    // Metoda zwraca wynik czy dany bus_new jest tym samym busem
     public boolean compareBus(Bus b_new, Bus b) {
         int x = (b_new.center.x - b.center.x) * (b_new.center.x - b.center.x);
         int y = (b_new.center.y - b.center.y) * (b_new.center.y - b.center.y);
-        System.out.println("Odległość od środka " + Math.sqrt((double) (x + y)));
+        // System.out.println("Odległość od środka " + Math.sqrt((double) (x + y)));
         return Math.sqrt((double) (x + y)) < delta;
     }
 
     public void newBus(Bus b_new) {
         for (Bus b : c_buses) {
-            if (b_new.line - b.line > but_lifetime) {
+            // Czy to jest ten sam bus ? 
+            if (compareBus(b_new, b)) {
+                b = b_new;              //aktualizacja danych o busie
+                System.out.println("Ten sam bus");
+                return;
+            }
+             // Jeśli bus w pamięci był ostatnio aktualizoawny później niż bus_lifetime
+            if (b_new.life_time - b.life_time > bus_lifetime) {
                 buses.add(b);
                 r_buses.add(b);
-                System.out.println("Będę usówał");
+                System.out.println("Będę usuwał");
                 // continue;
-            } else if (compareBus(b_new, b)) {
-                b = b_new;              //b_new is the same bus
-                System.out.println("Ten sam bus");
-                c_buses.removeAll(r_buses);
-                c_buses.add(b_new);  
-                return;
             }
         }
         c_buses.removeAll(r_buses);
-        System.out.println("dodaję busa " + b_new.line);
+        r_buses = new ArrayList<>();
+       // System.out.println("dodaję busa " + b_new.life_time);
         c_buses.add(b_new);             //if not adding new bus
 
     }
 
+    //Parser danych z pliku 
     public void input() {
         File file = new File("/home/jakub/darknet/mpk");
         try {
@@ -69,12 +72,16 @@ public class Yolo_project {
                     if (flag && !i.contains("%")) {
                         System.out.println(i + " " + line_counter);
                         String[] str = i.split(" ");
+                        //Tworzy busa 
                         Bus b = new Bus(Integer.parseInt(str[0]), Integer.parseInt(str[1]),
                                 Integer.parseInt(str[2]), Integer.parseInt(str[3]), line_counter);
+                        // Jeśli nie ma obecnie w pamieci żadnych busów dodaje busa 
                         if (c_buses.isEmpty()) {
                             c_buses.add(b);
-                            System.out.println("dodaję busa ");
+                            // System.out.println("dodaję busa ");
+
                         } else {
+                            // W przypadku gdy są jakieś busy sprawdza czy to nie jest ten sam metodą newBus
                             newBus(b);
                         }
                         flag = false;
